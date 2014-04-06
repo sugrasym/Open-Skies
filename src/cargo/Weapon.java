f760b5e7b47dc99ac8bf3abefaa600ad167cf2ec
Nathan Wiehoff <masternerdguy@yahoo.com>
2014-04-05 21:24:47 -0400

    * projectile skeleton in progress

--- src/cargo/Weapon.java
+++ src/cargo/Weapon.java
@@ -1,23 +1,26 @@
 /*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /*
  * Now for some meat. This class represents a turret.
  */
 package cargo;
 
+import celestial.Projectile;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
 import entity.Entity;
 import java.util.ArrayList;
 import lib.astral.Parser;
@@ -27,11 +30,13 @@ import lib.astral.Parser;
  * @author Nathan Wiehoff
  */
 public class Weapon extends Equipment {
+
     int width;
     int height;
     //weapon properties
-    protected double damage;
-    protected double speed;
+    private float shieldDamage;
+    private float hullDamage;
+    private float speed;
 
     public Weapon(String name) {
         super(name);
@@ -55,13 +60,14 @@ public class Weapon extends Equipment {
         if (relevant != null) {
             setName(relevant.getValue("name"));
             setType(relevant.getValue("type"));
-            setMass(Double.parseDouble(relevant.getValue("mass")));
-            setDamage(Double.parseDouble(relevant.getValue("damage")));
-            setRange(Double.parseDouble(relevant.getValue("range")));
-            setSpeed(Double.parseDouble(relevant.getValue("speed")));
-            setCoolDown(Double.parseDouble(relevant.getValue("refire")));
+            setMass(Float.parseFloat(relevant.getValue("mass")));
+            setShieldDamage(Float.parseFloat(relevant.getValue("shieldDamage")));
+            setHullDamage(Float.parseFloat(relevant.getValue("hullDamage")));
+            setRange(Float.parseFloat(relevant.getValue("range")));
+            setSpeed(Float.parseFloat(relevant.getValue("speed")));
+            setCoolDown(Float.parseFloat(relevant.getValue("refire")));
         } else {
-            System.out.println("Hades: The item " + getName() + " does not exist in WEAPONS.txt");
+            System.out.println("Error: The item " + getName() + " does not exist in WEAPONS.txt");
         }
     }
 
@@ -69,29 +75,68 @@ public class Weapon extends Equipment {
     public void activate(Entity target) {
         if (getCoolDown() <= getActivationTimer() && enabled) {
             setActivationTimer(0); //restart cooldown
-            fire();
+            //determine if OOS or not
+            if (host.getCurrentSystem() == host.getCurrentSystem().getUniverse().getPlayerShip().getCurrentSystem()) {
+                fire(target);
+            } else {
+                oosFire(target);
+            }
         }
     }
 
-    private void fire() {
+    private void fire(Entity target) {
+        /*
+         * This is the one called in system. It uses the physics system and is
+         * under the assumption that the host has been fully constructed. It
+         * generates a projectile using precached values, assigns those values,
+         * and finally drops the projectile into space.
+         */
         if (enabled) {
-            //TODO: FIRE WEAPON PROJECTILE
+            //generate projectile
+            Projectile pro = new Projectile(host.getCurrentSystem().getUniverse(), getName());
+            //store stats
+            pro.setShieldDamage(shieldDamage);
+            pro.setHullDamage(hullDamage);
+            pro.setSpeed(speed);
+            pro.setRange(speed);
+            //determine world location and rotation
+            Vector3f loc = getSocket().getNode().getWorldTranslation();
+            Quaternion rot = getSocket().getNode().getWorldRotation();
+            //interpolate velocity
+            
+            //store physics
+            pro.setLocation(loc);
+            pro.setRotation(rot);
         }
     }
 
-    public double getDamage() {
-        return damage;
+    private void oosFire(Entity target) {
+        if (enabled) {
+            //TODO: DEAL DAMAGE TO TARGET DIRECTLY
+        }
     }
 
-    public void setDamage(double damage) {
-        this.damage = damage;
+    public float getShieldDamage() {
+        return shieldDamage;
     }
 
-    public double getSpeed() {
+    public void setShieldDamage(float shieldDamage) {
+        this.shieldDamage = shieldDamage;
+    }
+
+    public float getHullDamage() {
+        return hullDamage;
+    }
+
+    public void setHullDamage(float hullDamage) {
+        this.hullDamage = hullDamage;
+    }
+
+    public float getSpeed() {
         return speed;
     }
 
-    public void setSpeed(double speed) {
+    public void setSpeed(float speed) {
         this.speed = speed;
     }
 }
---
+++ src/celestial/Projectile.java
@@ -0,0 +1,119 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Defines a projectile. Projectiles are a particle effect that represents a
+ * shot fired from a gun.
+ */
+package celestial;
+
+import com.jme3.effect.ParticleEmitter;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
+import lib.astral.Parser.Term;
+import universe.Universe;
+
+/**
+ *
+ * @author nwiehoff
+ */
+public class Projectile extends Celestial {
+    //particle effect
+
+    transient ParticleEmitter emitter;
+    //stats
+    private float shieldDamage;
+    private float hullDamage;
+    private float speed;
+    private float range;
+    //lifetime counter
+    private float diff = 0;
+
+    public Projectile(Universe universe, String name) {
+        super(0, universe);
+        setName(name);
+        //initStats(type);
+        deploy();
+    }
+
+    /*private void initStats(Term type) {
+        //store damage
+        shieldDamage = Float.parseFloat(type.getValue("shieldDamage"));
+        hullDamage = Float.parseFloat(type.getValue("hullDamage"));
+        //store lifetime
+        speed = Float.parseFloat(type.getValue("speed"));
+        range = Float.parseFloat(type.getValue("range"));
+    }*/
+
+    /*
+     * This method is called to actually deploy the projectile into the scene
+     */
+    private void deploy() {
+    }
+
+    /*
+     * OOS, weapons always hit and no projectile is generated. This means that
+     * the only one we have to worry about is alive().
+     */
+    @Override
+    protected void alive() {
+        //increment lifespan
+        diff += getVelocity().length() * tpf;
+        //has the projectile exceeded its max range?
+        if (diff >= range) {
+            //yep
+            setState(State.DYING);
+        } else {
+            //nope
+        }
+    }
+
+    @Override
+    protected void dying() {
+        setState(State.DEAD);
+    }
+
+    public float getShieldDamage() {
+        return shieldDamage;
+    }
+
+    public void setShieldDamage(float shieldDamage) {
+        this.shieldDamage = shieldDamage;
+    }
+
+    public float getHullDamage() {
+        return hullDamage;
+    }
+
+    public void setHullDamage(float hullDamage) {
+        this.hullDamage = hullDamage;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public float getRange() {
+        return range;
+    }
+
+    public void setRange(float range) {
+        this.range = range;
+    }
+}
